a423abe8fc619698130c3e8ccdd597eea6ecc179
farkas1070 <71120991+farkas1070@users.noreply.github.com>
2023-06-19 18:06:26 +0200

    added solutions

--- javascriptmegoldas.js
+++ javascriptmegoldas.js
@@ -1,7 +1,7 @@
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-
+ 
 function generateGraphWithSpecialLamps(normalLampCount, specialLampCount) {
   const graph = {};
   let starterNode = 'Egyetem';
