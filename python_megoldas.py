0381e8bb3f661587750ad618e7fe8bcf953c31aa
farkas1070 <71120991+farkas1070@users.noreply.github.com>
2023-07-06 15:50:18 +0200

    Revert "push"
    This reverts commit d02fee7d8a4b20cd77a905bffe8e4d9dfdb39e3f.

--- python_megoldas.py
+++ python_megoldas.py
@@ -1,6 +1,7 @@
 import json
 import random
 
+
 def random_number(min_val, max_val):
     return random.randint(min_val, max_val)
 
@@ -54,53 +55,54 @@ def select_random_elements(array, count):
 def find_shortest_path(graph, start, end, max_lamps):
     queue = []
     distances = {}
-    lamps_touched = {}
+    visited = {}
     path = {}
 
     for node in graph:
         distances[node] = float('inf')
-        lamps_touched[node] = 0
+        visited[node] = False
         path[node] = None
 
     distances[start] = 0
-    lamps_touched[start] = 0
-    queue.append((distances[start], lamps_touched[start], start))
+    queue.append(start)
 
     while queue:
-        queue.sort()
-        current_distance, current_lamps, current_node = queue.pop(0)
+        current_node = queue.pop(0)
+        visited[current_node] = True
 
         if current_node == end:
             break
-
-        if current_distance > distances[current_node] or current_lamps > lamps_touched[current_node]:
-            continue
 
         neighbors = graph[current_node]
 
         for neighbor, distance in neighbors.items():
             new_distance = distances[current_node] + distance
-            new_lamps = lamps_touched[current_node] + (neighbor.startswith('Specialnode'))
 
-            if new_distance < distances[neighbor] and new_lamps <= max_lamps:
+            if new_distance < distances[neighbor] and not visited[neighbor]:
                 distances[neighbor] = new_distance
-                lamps_touched[neighbor] = new_lamps
                 path[neighbor] = current_node
-                queue.append((distances[neighbor], lamps_touched[neighbor], neighbor))
+                queue.append(neighbor)
 
     if path[end] is None:
         return None
 
     shortest_path = []
     current_node = end
+    lamps = 0
 
     while current_node != start:
-        shortest_path.insert(0, current_node)
+        if current_node.startswith('Specialnode') and lamps < max_lamps:
+            shortest_path.insert(0, 'Specialnode')
+            lamps += 1
+        else:
+            shortest_path.insert(0, current_node)
+
         current_node = path[current_node]
 
     shortest_path.insert(0, start)
 
     return shortest_path
+
 
 def export_graph_to_json(graph, file_name):
     with open(file_name, 'w') as file:
