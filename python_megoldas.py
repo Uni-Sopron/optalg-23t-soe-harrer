17ed67582e00d4bde93dc73ee96dc92f04cd4e05
farkas1070 <71120991+farkas1070@users.noreply.github.com>
2023-07-06 15:54:15 +0200

    added correct python solution to correct branch

--- python_megoldas.py
+++ python_megoldas.py
@@ -1,6 +1,6 @@
 from collections import deque
 import random
-
+import json
 
 def findShorthestPath(graph, start, end, max_lamps):
     """ 
@@ -64,57 +64,65 @@ def findShorthestPath(graph, start, end, max_lamps):
     return shortest_path
 
 
-def generate_graph_with_special_lamps(normal_lamp_count, special_lamp_count):
-    graph = {}
-    starter_node = 'Egyetem'
-    finish_node = 'Harrer'
+def find_shortest_path(graph, start, end, max_lamps):
+    queue = []
+    distances = {}
+    lamps_touched = {}
+    path = {}
 
-    # Normális lámpás csúcsok generálása
-    for i in range(1, normal_lamp_count + 1):
-        if i == 1:
-            node_name = starter_node
-            graph[node_name] = {}
-        elif i < normal_lamp_count:
-            node_name = f"Node{i}"
-            graph[node_name] = {}
+    for node in graph:
+        distances[node] = float('inf')
+        lamps_touched[node] = 0
+        path[node] = None
 
-    # Speciális lámpás csúcsok generálása
-    for i in range(1, special_lamp_count + 1):
-        node_name = f"Specialnode{i}"
-        graph[node_name] = {}
+    distances[start] = 0
+    lamps_touched[start] = 0
+    queue.append((distances[start], lamps_touched[start], start))
 
-    node_name = finish_node
-    graph[node_name] = {}
+    while queue:
+        queue.sort()
+        current_distance, current_lamps, current_node = queue.pop(0)
 
-    # Élek generálása
-    nodes = list(graph.keys())
+        if current_node == end:
+            break
 
-    for i in range(len(nodes)):
-        current_node = nodes[i]
-        neighbors = nodes[i+1:]  # Csak a későbbi csúcsok között generál éleket
+        if current_distance > distances[current_node] or current_lamps > lamps_touched[current_node]:
+            continue
 
-        number_of_edges = random.randint(
-            0, len(neighbors))  # Véletlenszerű élek száma
-        # Véletlenszerűen kiválasztott szomszédok
-        selected_neighbors = select_random_elements(neighbors, number_of_edges)
+        neighbors = graph[current_node]
 
-        for j in range(len(selected_neighbors)):
-            neighbor_node = selected_neighbors[j]
-            weight = random.randint(1, 5)
-            if current_node not in graph:
-                graph[current_node] = {}
-            graph[current_node][neighbor_node] = weight
+        for neighbor, distance in neighbors.items():
+            new_distance = distances[current_node] + distance
+            new_lamps = lamps_touched[current_node] + (neighbor.startswith('Specialnode'))
 
-    return graph
+            if new_distance < distances[neighbor] and new_lamps <= max_lamps:
+                distances[neighbor] = new_distance
+                lamps_touched[neighbor] = new_lamps
+                path[neighbor] = current_node
+                queue.append((distances[neighbor], lamps_touched[neighbor], neighbor))
 
+    if path[end] is None:
+        return None
+
+    shortest_path = []
+    current_node = end
+
+    while current_node != start:
+        shortest_path.insert(0, current_node)
+        current_node = path[current_node]
+
+    shortest_path.insert(0, start)
+
+    return shortest_path
 
 def select_random_elements(array, count):
     shuffled = array[:]
     random.shuffle(shuffled)
     return shuffled[:count]
 
+f = open('graph1.json')
+graph = json.load(f)
 
-graph = generate_graph_with_special_lamps(5, 2)
 start_node = 'Egyetem'
 end_node = 'Harrer'
 max_lamps = 2
