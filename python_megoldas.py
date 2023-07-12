c17fd255cf6d78934e02dcd47dba6ffbedb0370f
Dávid Borsodi <smarton0614@gmail.com>
2023-07-12 09:20:42 +0200

    lehetséges megoldás

--- python_megoldas.py
+++ python_megoldas.py
@@ -7,36 +7,24 @@ from collections import deque
 
 
 def findShorthestPath(graph, start, end, max_lamps):
-    """
-    Legrövidebb útvonalat keres az adott gráfban a kezdőponttól a végpontig, figyelembe véve a maximális lámpás kereszteződések számát.
-
-    Args:
-        graph (dict): A gráf reprezentációja, amely a csúcsok és azok közötti élek tárolására szolgál.
-        start (str): A kezdőcsúcs neve.
-        end (str): A végcsúcs neve.
-        max_lamps (int): A maximális lámpás kereszteződések száma, amelyet az útvonalban engedélyezünk.
-
-    Returns:
-        list or None: A legrövidebb útvonal csúcsok listájaként, vagy None, ha nincs elérhető útvonal.
-    """
     queue = []
     distances = {}
     visited = {}
     path = {}
 
     for node in graph:
-        distances[node] = float("inf")
+        for lamps in range(max_lamps + 1):
+            distances[(node, lamps)] = float("inf")
         visited[node] = False
         path[node] = None
 
-    distances[start] = 0
+    distances[(start, 0)] = 0
     queue.append((start, 0))
 
     while queue:
-        # Find the node with the minimum distance in the queue
         min_index = 0
         for i in range(1, len(queue)):
-            if distances[queue[i][0]] < distances[queue[min_index][0]]:
+            if distances[queue[i]] < distances[queue[min_index]]:
                 min_index = i
 
         current_node, lamps = queue.pop(min_index)
@@ -48,31 +36,33 @@ def findShorthestPath(graph, start, end, max_lamps):
         neighbors = graph[current_node]
 
         for neighbor, distance in neighbors.items():
-            new_distance = distances[current_node] + distance
+            new_distance = distances[(current_node, lamps)] + distance
 
-            if new_distance < distances[neighbor] and not visited[neighbor]:
-                distances[neighbor] = new_distance
+            if new_distance < distances.get((neighbor, lamps), float("inf")) and not visited[neighbor]:
+                distances[(neighbor, lamps)] = new_distance
                 path[neighbor] = current_node
                 queue.append((neighbor, lamps))
 
     if path[end] is None:
         return None
-
+    
     shortest_path = []
     current_node = end
+    
 
-    while current_node != start:
-        if (
-            graph[current_node]
-            and "Specialnode" in graph[current_node]
-            and lamps < max_lamps
-        ):
+    #special node nem lehet nagyobb mint 2
+    while current_node != start and lamps <= 2:
+        if (graph[current_node] and "Specialnode" in graph[current_node] and lamps <= max_lamps):
             shortest_path.insert(0, ("Specialnode", lamps + 1))
             lamps += 1
+            
         shortest_path.insert(0, (current_node, lamps))
         current_node = path[current_node]
 
     shortest_path.insert(0, (start, lamps))
+
+    if lamps <= 2 and current_node != start:
+        shortest_path.insert(0, (current_node, lamps))
 
     return shortest_path
 
